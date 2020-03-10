809933f13aa5098892795ff2c1ff83d53e6c6c67
lechestercat <chesterortega1991@gmail.com>
2020-03-09 22:31:43 -0400

    Add ability to edit, add and delete techs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import TechSelectOptions from '../techs/TechSelectionOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -66,9 +67,7 @@ const EditLogModal = ({ current, updateLog }) => {
             <option value='' disabled>
               Select Technican
             </option>
-            <option value='John Doe'>John Doe</option>
-            <option value='Sam Smith'>Sam Smith</option>
-            <option value='Sara Wilson'>Sara Wilson</option>
+            <TechSelectOptions />
           </select>
         </div>
       </div>
--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
   const onSubmit = () => {
     if (firstName === '' || lastName === '') {
-      M.toast({ html: 'Please enter the first ans last name' });
+      M.toast({ html: 'Please enter the first and last name' });
     } else {
-      console.log(firstName, lastName);
+      addTech({
+        firstName,
+        lastName
+      });
 
-      // clear fields
+      M.toast({ html: `${firstName} ${lastName} was added as a tech` });
+
+      // Clear Fields
       setFirstName('');
       setLastName('');
     }
@@ -20,7 +28,7 @@ const AddTechModal = () => {
   return (
     <div id='add-tech-modal' className='modal'>
       <div className='modal-content'>
-        <h4>New Technican</h4>
+        <h4>New Technician</h4>
         <div className='row'>
           <div className='input-field'>
             <input
@@ -53,7 +61,7 @@ const AddTechModal = () => {
         <a
           href='#!'
           onClick={onSubmit}
-          className='modal-close waves-effect waves-light blue btn'
+          className='modal-close waves-effect blue waves-light btn'
         >
           Enter
         </a>
@@ -62,4 +70,8 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired
+};
+
+export default connect(null, { addTech })(AddTechModal);
